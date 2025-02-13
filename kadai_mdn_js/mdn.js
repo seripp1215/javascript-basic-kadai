86d803e8f1e1490a3829b4505c4914a57f82b5e5
seripp1215 <seripp18@gmail.com>
2025-02-14 00:29:50 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const year = today.getFullYear();
+
+const monthIndex = today.getMonth() + 1;
+
+const day = today.getDate();
+
+console.log(year + '年', monthIndex + '月', day + '日');
